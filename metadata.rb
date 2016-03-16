c17d6661353f0ef2499ad0944f881c602269d867
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-03-16 16:03:19 +0000

    chore(version) - bumps version to 1.4.0

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name                "solr"
-version             "1.3.0"
+version             "1.4.0"
 description         "Install and configure Solr. Defaults to version 5.3"
 maintainer          "Markup"
 depends             "java"
