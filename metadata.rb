b7e0a921aca0d12f748fe303f50f32163e39f933
Gavin Staniforth <gavin@usemarkup.com>
2018-03-31 18:07:42 +0100

    chore: prevent start as solr isnt configured

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name                'solr'
-version             '3.0.2'
+version             '3.0.3'
 description         'Install and configure Solr. Defaults to version 5.3'
 maintainer          'Markup'
 
