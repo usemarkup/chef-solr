00a94787f1a077028b75b4d7916cb21728492231
Gavin Staniforth <gavin@usemarkup.com>
2018-03-31 17:31:33 +0100

    chore: release 3.0.2

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name                'solr'
-version             '3.0.1'
+version             '3.0.2'
 description         'Install and configure Solr. Defaults to version 5.3'
 maintainer          'Markup'
 
