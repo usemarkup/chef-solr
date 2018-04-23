1e62ecb368f32a779a1fbfda3fe224db26da3b67
Gavin Staniforth <gsdevme@users.noreply.github.com>
2018-04-23 19:25:51 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name                'solr'
-version             '3.0.3'
+version             '3.0.4'
 description         'Install and configure Solr. Defaults to version 5.3'
 maintainer          'Markup'
 
