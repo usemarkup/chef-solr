c6232bc2fde18e0c2e62e31e3f1d5dcf1a49354c
Gavin Staniforth <gsdevme@users.noreply.github.com>
2018-04-29 20:59:11 +0100

    chore: release 3.10

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name                'solr'
-version             '3.0.4'
+version             '3.1.0'
 description         'Install and configure Solr. Defaults to version 5.3'
 maintainer          'Markup'
 
